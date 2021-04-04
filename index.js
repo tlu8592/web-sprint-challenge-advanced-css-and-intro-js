76e641f625c9b596f26f3b3961aaf24037a42c6e
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-04-04 12:10:57 -0400

    Test passed for Task 7: lotsOfArt, MVP-JavaScript completed

--- index.js
+++ index.js
@@ -301,12 +301,15 @@ Use lotsOfArt to do the following:
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/){
-  /*Your Code Here*/
+function lotsOfArt(arr){
+  let results = [];
+  for (let index in arr) {
+    if (arr[index]["paintings"] > 100) {
+      results.push(arr[index]["name"]);
+    }
+  }
+  return results;
 }
-
-
-
 
 // 🎨🎨 STRETCH 🎨🎨//
 /* 💪💪💪💪💪💪 STRETCH 1: 💪💪💪💪💪💪 
