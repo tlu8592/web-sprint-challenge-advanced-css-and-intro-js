ee8ed56c31f2c3b1f735881c21e98dbbfc3f0882
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-04-04 12:04:55 -0400

    Test passed for Task 6: addArtist

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { transformSync } from "@babel/core";
+
 export const artists = [
     {
       "id": 0,
@@ -280,11 +282,17 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/){
-    /*Your Code Here*/
-  }
-
-  
+function addArtist(arr){
+    arr.push({
+      id: 20,
+      name: "Tony",
+      years: "1992-present",
+      genre: "Web Design",
+      nationality: "American",
+      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget rhoncus quam, rutrum sollicitudin ante."
+    });
+    return arr;
+}
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use lotsOfArt to do the following: 
