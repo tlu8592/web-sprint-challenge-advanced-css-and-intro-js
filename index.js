a2625d99968c8e8aad0c9d13f7e17fa2c99b5cda
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-04-04 11:58:14 -0400

    Test passed for Task 5: removeArtist

--- index.js
+++ index.js
@@ -259,11 +259,11 @@ function get20s(arr){
  
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/){
-   /*Your Code Here*/
+function removeArtist(arr, idx){
+   arr.splice(idx, 1);
+   return arr.length;
 }
    
-
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use addArtist to do the following: 
 1. Receive an array
